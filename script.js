1fbbd3ab7001236b314aca5eb32c895835cf7ff5
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 16:32:55 +0500

    task 4 from chapter 5.11 solved in new branch 5.11_task_4

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 //task 1
-let date = new Date(2012,2,20,3,12,0,0)
-console.log(date);
+// let date = new Date(2012,2,20,3,12,0,0)
+// console.log(date);
+
+//task 4
+let date = new Date(2015, 0, 2);
+function getDateAgo(date, days) {
+    let a = +date - (days * 24 * 3600 * 1000);
+    return new Date(a);
+}
+console.log( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+console.log( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+console.log( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
