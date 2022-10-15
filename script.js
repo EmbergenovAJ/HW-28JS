dfdf7c338e7e54ab05c0c45ebefca836c0ada5e8
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 17:10:59 +0500

    task 7 from chapter 5.11 solved in new branch 5.11_task_7

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 //task 1
-let date = new Date(2012,2,20,3,12,0,0)
-console.log(date);
+// let date = new Date(2012,2,20,3,12,0,0)
+// console.log(date);
+
+//task 7
+function getSecondsToTomorrow(){
+    let date = new Date();
+    let tomorrow = new Date(+date + 24*3600*1000);
+    tomorrow.setHours(0, 0, 0,0)
+    console.log(date)
+    console.log(tomorrow)
+    return Math.round((+tomorrow - +date)/1000 )
+}
+
+console.log( getSecondsToTomorrow() );
