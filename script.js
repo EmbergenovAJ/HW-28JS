e47799c4b591732cf5cd2a7ea5e7febe3acb5929
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 15:25:14 +0500

    task 2 from chapter 5.10 solved in branch capter5.10

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 //task 1
-let user = {
-    name: "John",
-    years: 30,
+// let user = {
+//     name: "John",
+//     years: 30,
+// };
+// let {name, years, isAdmin = false} = user;
+// console.log(name);
+// console.log(years);
+// console.log(isAdmin);
+
+
+//task 2 from 5.10
+let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
 };
-let {name, years, isAdmin = false} = user;
-console.log(name);
-console.log(years);
-console.log(isAdmin);
+
+function topSalary( obj ) {
+    let arr = ['Din', 0];
+    for (let [key, value] of Object.entries( obj )){
+        if (arr[1] <  value) arr = [key, value];
+    }
+    return arr[0];
+}
+
+console.log(topSalary(salaries));
