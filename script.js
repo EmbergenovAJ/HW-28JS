9a868cc5d2d14ad4f3602e966a03a99031ead6a4
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 17:55:01 +0500

    task 2 from chapter 5.12 was solved 5.12_task_2 branch

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 //task 1
-let user = {
-    name: "Василий Иванович",
-    age: 35
+// let user = {
+//     name: "Василий Иванович",
+//     age: 35
+// };
+//
+// let a = JSON.parse( JSON.stringify(user) );
+// console.log(a);
+// console.log(JSON.stringify(user))
+
+//task 2
+
+let room = {
+    number: 23
 };
 
-let a = JSON.parse( JSON.stringify(user) );
-console.log(a);
-console.log(JSON.stringify(user))
+let meetup = {
+    title: "Совещание",
+    occupiedBy: [{name: "Иванов"}, {name: "Петров"}],
+    place: room
+};
+
+// цикличные ссылки
+room.occupiedBy = meetup;
+meetup.self = meetup;
+
+console.log( JSON.stringify( meetup, function replacer(key, value) {
+    return (key !== '' && value == meetup) ? undefined : value;
+} ) );
