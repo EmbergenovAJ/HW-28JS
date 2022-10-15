cf734542bde330d3a66ac6c0f1cea3471c5ad4de
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 19:00:00 +0500

    task 4 from chapter 6.1 was solved in branch 6.1_task_4

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
 // task 1
-function sumTo(n){
-    // let res = 0;
-    // for (let i = 0; i <=n; i++) {
-    //     res += i;
+// function sumTo(n){
+//     // let res = 0;
+//     // for (let i = 0; i <=n; i++) {
+//     //     res += i;
+//     // }
+//     // return res;
+//
+//     // return n/2*(n+1)
+//
+//     if (n === 1) return 1;
+//     return n + sumTo(n - 1);
+// }
+//
+// console.log(sumTo(100))
+
+//task 4
+let list = {
+    value: 1,
+    next: {
+        value: 2,
+        next: {
+            value: 3,
+            next: {
+                value: 4,
+                next: null
+            }
+        }
+    }
+};
+
+function printList( list ) {
+    // while (list){
+    //     console.log(list.value);
+    //     list = list.next;
     // }
-    // return res;
 
-    // return n/2*(n+1)
-
-    if (n === 1) return 1;
-    return n + sumTo(n - 1);
+    console.log(list.value);
+    if (list.next) {
+        printList(list.next);
+    }
 }
 
-console.log(sumTo(100))
+printList(list);
