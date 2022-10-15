3751191735deb84df7b5a956b7b50f62e44b4d53
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 14:52:45 +0500

    task 2 solved in new branch task_2

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 //task 1
-let salaries = {
-    "John": 100,
-    "Pete": 300,
-    "Mary": 250,
-};
+// let salaries = {
+//     "John": 100,
+//     "Pete": 300,
+//     "Mary": 250,
+// };
+//
+// function sumSalaries( obj ) {
+//     if (Object.keys( obj )[0] === undefined) return 0;
+//     let summ = 0;
+//     for (let value of Object.values( obj )){
+//         summ += value;
+//     }
+//     return summ;
+// }
+// console.log ( sumSalaries(salaries) );
 
-function sumSalaries( obj ) {
-    if (Object.keys( obj )[0] === undefined) return 0;
-    let summ = 0;
-    for (let value of Object.values( obj )){
-        summ += value;
-    }
-    return summ;
+// task 2
+let user = {
+    name: 'John',
+    age: 30,
+    isAdmin: false,
+};
+function count( obj ) {
+    return Object.keys( obj ).length;
 }
-console.log ( sumSalaries(salaries) );
+console.log( count(user) );
