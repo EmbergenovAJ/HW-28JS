137c9d6eb1aa92f049976c9d965d848b632f2aa4
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 18:14:04 +0500

    task 2 from chapter 6.1 was solved in branch 6.1_task_2

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 // task 1
-function sumTo(n){
-    // let res = 0;
-    // for (let i = 0; i <=n; i++) {
-    //     res += i;
-    // }
-    // return res;
+// function sumTo(n){
+//     // let res = 0;
+//     // for (let i = 0; i <=n; i++) {
+//     //     res += i;
+//     // }
+//     // return res;
+//
+//     // return n/2*(n+1)
+//
+//     if (n === 1) return 1;
+//     return n + sumTo(n - 1);
+// }
+//
+// console.log(sumTo(100))
 
-    // return n/2*(n+1)
-
+//task 2
+function factorial(n){
     if (n === 1) return 1;
-    return n + sumTo(n - 1);
+    return n * factorial(n - 1 );
 }
 
-console.log(sumTo(100))
+console.log(factorial(5))
