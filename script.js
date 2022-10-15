25a13f7ea30905a09fd3c2cdd444493db63ee8ca
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 16:55:15 +0500

    task 56 from chapter 5.11 solved in new branch 5.11_task_6

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 //task 1
-let date = new Date(2012,2,20,3,12,0,0)
-console.log(date);
+// let date = new Date(2012,2,20,3,12,0,0)
+// console.log(date);
+
+//task 6
+function getSecondsToday () {
+    let date = new Date();
+    return date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds();
+}
+
+console.log(getSecondsToday());
