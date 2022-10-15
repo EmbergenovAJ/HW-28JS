4addcea1534adf0804bbcf5dc1fe1b94c5e87210
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 16:19:32 +0500

    task 2 from chapter 5.11 solved in new branch 5.11_task_2

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 //task 1
-let date = new Date(2012,2,20,3,12,0,0)
-console.log(date);
+// let date = new Date(2012,2,20,3,12,0,0)
+// console.log(date);
+
+//task 2
+let date = new Date(2012, 0, 3);
+
+function getWeekDay( date ) {
+    let a = date.getDay();
+    switch (a){
+        case 0: return 'ВС'
+        case 1: return 'ПН'
+        case 2: return 'ВТ'
+        case 3: return 'СР'
+        case 4: return 'ЧТ'
+        case 5: return 'ПТ'
+        case 6: return 'СБ'
+    }
+}
+
+console.log( getWeekDay(date) );
