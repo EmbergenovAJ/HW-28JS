a194b8d2d9af87d22b717f6e2403f04aa62fbce7
AydosEmbergenov <19ayda967198@gmail.com>
2022-10-15 16:24:35 +0500

    task 3 from chapter 5.11 solved in new branch 5.11_task_3

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 //task 1
-let date = new Date(2012,2,20,3,12,0,0)
-console.log(date);
+// let date = new Date(2012,2,20,3,12,0,0)
+// console.log(date);
+
+//task 3
+let date = new Date(2012, 0, 3);
+function getLocalDay( date ){
+    let day = date.getDay();
+    if (day === 0) return 7;
+    return day;
+}
+console.log( getLocalDay(date) );
